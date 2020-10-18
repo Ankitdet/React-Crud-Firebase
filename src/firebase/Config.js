162f76cb47b2ebe91468c8a6d402aad35d2de0ea
ankit <ankitdetroja@gmail.com>
2020-10-18 13:04:37 +0530

    Second commit.

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -4,7 +4,6 @@ import 'firebase/firestore'
 import 'firebase/storage'
 import 'firebase/auth'
 
-// 特別注意，在react.js中雖然可以直接使用env內的環境變數，但是每個環境變數都要前綴REACT_APP才能用
 let firebaseConfig = {
   apiKey: "AIzaSyAyUx3ijQmPBq3qoFBzKBDRbUJ0n3kf-oQ",
   authDomain: "seller-store.firebaseapp.com",
@@ -14,7 +13,7 @@ let firebaseConfig = {
   messagingSenderId: "277033873614",
   appId: "1:277033873614:web:64d927eb3184cc4f68a09f"
 }
-// 初始化 Firebase
+
 firebase.initializeApp(firebaseConfig)
 
 export const projectDB = firebase.database().ref()
@@ -23,12 +22,10 @@ export const projectStorage = firebase.storage()
 export const projectFirestore = firebase.firestore()
 export const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-// 使用google註冊
 export const projectAuthGoogle = new firebase.auth.GoogleAuthProvider()
 projectAuthGoogle.setCustomParameters({
   promt: "select_account",
 })
-// 使用email跟password註冊
 export const signUpWithEmail = (email, password) =>
   projectAuth.createUserWithEmailAndPassword(email, password)
 
@@ -55,11 +52,8 @@ export const createUserProfileDocument = async (userAuth) => {
   }
 }
 
-// 用google登入
 export const signInWithGoogle = () => projectAuth.signInWithPopup(projectAuthGoogle)
-// 使用email跟password登入
 export const signInWithEmail = (email, password) => projectAuth.signInWithEmailAndPassword(email, password)
 export const signOut = () => projectAuth.signOut()
-// 重設email跟pasword
 export const resetEmail = (email) => projectAuth.sendPasswordResetEmail(email);
 export const resetPassword = (password) => projectAuth.currentUser.updatePassword(password)
--- src/functionComponents/ContactsFunction.js
+++
@@ -1,5 +0,0 @@
-import contactsFormContent from '../apiComponents/ContactsFormAPI'
-
-export const getContactsFormContent = () => {
-  return contactsFormContent
-}
